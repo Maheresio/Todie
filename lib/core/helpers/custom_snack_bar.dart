43e1e0c613efac33d4d167d306601210ca376338
Maheresio <zamalek.545r5@gmail.com>
2024-12-27 19:28:15 +0200

    update custom snack bar ui

--- lib/core/helpers/custom_snack_bar.dart
+++ lib/core/helpers/custom_snack_bar.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 void displaySnackBar(context, {required content}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      
-      content: Text(
-        content,
+      backgroundColor: Colors.red.shade300,
+      content: Center(
+        child: Text(
+          content,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
       ),
     ),
   );
